a1e0424a2bf5e4c2db9756c4850adeef0fae6a58
JoshBroomberg <joshbroomberg@gmail.com>
2015-07-06 00:34:12 +0200

    Upgrade to messaging system to employ DRY and utilise new schema

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,30 +2,50 @@ class MessagesController < ApplicationController
 	skip_before_filter  :verify_authenticity_token
 	#apply DRY principle
 	def create
-        closed = false
+		closed = false
 		if params["To"] == "+12316468691"
 			vendor = Vendor.where(number: "+12316468691")[0] #chnage to have number from 
 			category = "PI" #purchase init
-			message = vendor.messages.build(body: params["Body"], category: category, closed: closed)
-			if message.save
-			reply("This is a Pi reply", "+27836538932", "Josh")
-			end
+			saveMessage(vendor, category, params["Body"])
 		elsif params["To"] == "+16123612985"
 			vendor = Vendor.where(number: "+12316468691")[0] #chnage to have number from
 			category = "DI" #deposit init
-			message = vendor.messages.build(body: params["Body"], category: category, closed: closed)
-			if message.save
-			reply("This is a Di reply", "+27836538932", "Josh")
-			end
+			saveMessage(vendor, category, params["Body"])
 		elsif params["To"] == "+16123613027"
 			user = User.where(number: "+12316468691")[0]
 			category = "Conf" #confirm
-			message = user.messages.build(body: params["Body"], category: category, closed: closed)
-			if message.save
-			reply("This is a conf reply", "+27836538932", "Josh")
-			end
+			saveMessage(user, category, params["Body"], params["From"])
 		end
 		
+	end
+
+	def saveMessage(obj, category, body, senderNumber)
+		if body.index(" ") != nil
+			parts = body.split(" ")
+			if parts[0].length==4
+				parts[1] = parts[1].gsub(",", ".")
+				if isNumeric parts[1]
+					message = obj.messages.build(account_id: parts[0], amount: parts[1], category: category, closed: closed)
+					if message.save
+						#number must be desired customer number based on acc_id
+						reply("This is a #{category} reply", "+27836538932", "Josh")
+					end
+				else
+					reply("Please ensure the amount you entered is a digit", senderNumber, "Josh")
+				end
+
+
+
+			else
+				reply("AccID must be 4 characters", senderNumber, "Josh")
+			end
+		else
+			reply("Please separate AccID and amount with a space", senderNumber, "Josh")
+		end
+	end
+
+	def isNumeric number
+		Float(number) != nil rescue false
 	end
 
 
