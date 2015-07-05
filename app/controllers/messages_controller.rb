1e6d5d1ad64c9f42fabd490227c00f5adfd4cbed
JoshBroomberg <joshbroomberg@gmail.com>
2015-07-05 22:28:26 +0200

    messaging system v0.4.0 - message system differentiated for 3 numberss

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,21 @@
 class MessagesController < ApplicationController
 	skip_before_filter  :verify_authenticity_token
-	def createsale
+	def create
+        closed = false
+		if params["To"] == "+12316468691"
+			type = "PI" #purchase init
+			Vendor.messages.build(body: params["Body"], type: type, closed: closed)
+		elsif params["To"] == "+16123612985"
+			type = "DI" #deposit init
+			Vendor.messages.build(body: params["Body"], type: type, closed: closed)
+		elsif params["To"] == "+16123612985"
+			type = "Conf" #confirm
+			User.messages.build(body: params["Body"], type: type, closed: closed)
+		end
+
+			
+
+
 		message = Message.new(body: params["Body"])
 		message.save
 		reply("This is a reply", "+27836538932", "Josh")
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
-  post "messages/sale/new" => "messages#createsale"
+  post "messages/new" => "messages#create"
 
   # Example of regular route:
   # get 'products/:id' => 'catalog#view'
