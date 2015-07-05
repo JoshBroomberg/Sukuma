895d82c62f4af5c8d418bb2736c5f8d8a293302b
JoshBroomberg <joshbroomberg@gmail.com>
2015-07-05 21:38:46 +0200

    messaging system v0.3.3 - reply capability

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,30 @@
 class MessagesController < ApplicationController
-    skip_before_filter  :verify_authenticity_token
+	skip_before_filter  :verify_authenticity_token
 	def createsale
 		message = Message.new(body: params["Body"])
 		message.save
+		require 'rubygems'
+		require 'twilio-ruby'
+
+		account_sid = "ACabc2a633d8052c4b8805de4b678f4166"
+		auth_token = "255b0b0cd465bbd6d0e3b3a32cd1ad4e"
+		client = Twilio::REST::Client.new account_sid, auth_token
+
+		from = "+12316468691" # Your Twilio number
+
+		friends = {
+			"+27836538932" => "Praise"
+		}
+		friends.each do |key, value|
+			client.account.messages.create(
+				:from => from,
+				:to => key,
+				:body => "Hey #{value}, how you doing"
+				)
+			puts "Sent message to #{value}"
+		end
 	end
+
+
 
 end
