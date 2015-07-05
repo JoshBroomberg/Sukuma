607cb89effb8f4c9e13b31861636ce7014bf16d2
JoshBroomberg <joshbroomberg@gmail.com>
2015-07-05 21:44:00 +0200

    messaging system v0.3.4 - reply capability bug fixs

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,9 +3,6 @@ class MessagesController < ApplicationController
 	def createsale
 		message = Message.new(body: params["Body"])
 		message.save
-		require 'rubygems'
-		require 'twilio-ruby'
-
 		account_sid = "ACabc2a633d8052c4b8805de4b678f4166"
 		auth_token = "255b0b0cd465bbd6d0e3b3a32cd1ad4e"
 		client = Twilio::REST::Client.new account_sid, auth_token
