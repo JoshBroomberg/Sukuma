4a9960c2217b97bf3eb6d3b68154e8b1668be124
JoshBroomberg <joshbroomberg@gmail.com>
2015-07-05 20:46:23 +0200

    message system v0.2

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,8 @@
 class MessagesController < ApplicationController
     skip_before_filter  :verify_authenticity_token
 	def createsale
-		message = Message.new(body: params["body"])
+		message = Message.new(body: params["Body"])
+		message.save
 	end
 
 end
