1068754cfb572a96d1ee98eef9a74770e9da6638
JoshBroomberg <joshbroomberg@gmail.com>
2015-07-06 22:30:21 +0200

    removed email field from devise

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,8 +1,31 @@
 class Client < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  
+  validates :number, length: { is: 12 }, uniqueness: true
+  #validates :bio, length: { maximum: 500 }
+
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable
+  attr_accessor :login
+
+  def self.find_for_database_authentication(warden_conditions)
+  	conditions = warden_conditions.dup
+  	if login = conditions.delete(:login)
+  		where(conditions.to_h).where(["lower(number) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+  	else
+  		where(conditions.to_h).first
+  	end
+  end
+
+  def email_required?
+  	false
+  end
+
+  def email_changed?
+  	false
+  end
 
   has_one :account
   has_one :profile
