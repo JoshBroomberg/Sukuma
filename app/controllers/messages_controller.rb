447b9a57113c336e23edb6a09409c39ccb0aae5e
JoshBroomberg <joshbroomberg@gmail.com>
2015-07-06 19:14:47 +0200

    bug fixing v2

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -48,7 +48,7 @@ class MessagesController < ApplicationController
 							if Transaction.where(customer_id: client.id, state: :waitingconfirm).count == 0 && Transaction.where(customer_id: client.id, state: :initiated).count == 0
 
 
-								transaction = Transaction.new(customer_id: client.id, vendor: user.id, amount: message.amount, state: :initiated, kind: kind)
+								transaction = Transaction.new(customer_id: client.id, vendor_id: user.id, amount: message.amount, state: :initiated, kind: kind)
 								transaction.save
 
 								balanceSuff= true
