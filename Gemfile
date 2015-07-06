b8cf0ddde9d95a717ad2307bae121016dc5dd844
JoshBroomberg <joshbroomberg@gmail.com>
2015-07-06 19:22:25 +0200

    bug fixes

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem "twitter-bootstrap-rails"
 #gem 'rubygems'
 gem 'twilio-ruby'
 gem "rails_12factor"
+gem "pry-rails"
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -86,7 +86,7 @@ class MessagesController < ApplicationController
 			#user = User.where(number: params["From"])[0]
 			useraccount = user.account
 			transaction  = Transaction.find_by(customer_id: user.id, state: :waitingconfirm)
-
+			binding.pry
 			if Transaction.where(customer_id: user.id, state: :waitingconfirm).count > 0
 				vendor = Client.find(transaction.vendor_id)
 			    vendoraccount = vendor.account
