7d4e876383bab9bbbf50ff4de7338051eead6a2b
JoshBroomberg <joshbroomberg@gmail.com>
2015-07-08 23:31:24 +0200

    further charting improvements

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -4,6 +4,8 @@ module AccountsHelper
 
 		percentagehash = {}
 		transactions = Transaction.where(:created_at => 1.months.ago..Time.now, customer_id: current_client.id, state: 3, kind: 0)
+		deposits = Transaction.where(:created_at => 1.months.ago..Time.now, customer_id: current_client.id, state: 3, kind: 1)
+		
 		sum = 0.0
 		p2psum = 0.0
 		prefixSum = {}
@@ -20,6 +22,18 @@ module AccountsHelper
 			end
 			counter+=1
 		end
+
+		prefixSumD = {}
+		counterD = 1
+		deposits.each do |deposit|
+			if prefixSumD.length>0
+				prefixSumD[counterD] = (prefixSumD[counterD-1]+deposit.amount)
+			else
+				prefixSumD[counterD]=(deposit.amount)
+			end
+			counterD+=1
+		end
+
 		Profile.categories.each do |key, value|
 			# catTrans = []
 			# transactions.each do |trans|
@@ -29,7 +43,7 @@ module AccountsHelper
 			# 	end
 			# end
 			catTrans = transactions.select{|transaction| Client.find(transaction.vendor_id).profile.category == key	}
-			catTrans = catTrans.reject{|trans| Client.find(trans.vendor_id).profile.class.name == "Cprofile"	 }
+			catTrans = catTrans.reject{|trans| Client.find(trans.vendor_id).profile.class.name == "Cprofile"}
 			subSum = 0.0
 			catTrans.each do |trans|
 				subSum+= trans.amount
@@ -54,9 +68,27 @@ module AccountsHelper
 			cnt+=1
 		end
 		prefixSum.keys.each { |k| prefixSum[ mappings[k] ] = prefixSum.delete(k) if mappings[k] }
-		prefixSum2 = {}
+		
+		# prefixSum2 = {}
+		# prefixSum.each do |key, value|
+		# 	prefixSum2[key] = current_client.account.balance
+		# end
+
+		mappings ={}
+		cnt = 1
+		prefixSumD.each do 
+			mappings[cnt] =  "Transaction "+cnt.to_s
+			cnt+=1
+		end
+		prefixSumD.keys.each { |k| prefixSumD[ mappings[k] ] = prefixSumD.delete(k) if mappings[k] }
+		
+		val = 0.0
 		prefixSum.each do |key, value|
-			prefixSum2[key] = current_client.account.balance
+			if !prefixSumD.include?(key)
+			   prefixSumD[key] = val
+			else
+				val = prefixSumD[key]
+			end
 		end
 
 
@@ -71,8 +103,18 @@ module AccountsHelper
 		end
 		transactions.keys.each { |k| transactions[ mappings[k] ] = transactions.delete(k) if mappings[k] }
 
+		deposits = deposits.group_by_day(:created_at).count
+		mappings ={}
+		oldkeys = deposits.keys
+		cnt = 0
+		deposits.each do 
+			mappings[oldkeys[cnt]] =  oldkeys[cnt].to_s[0...10]
+			cnt+=1
+		end
+		deposits.keys.each { |k| deposits[ mappings[k] ] = deposits.delete(k) if mappings[k] }
+
 		#binding.pry
-		[percentagehash, prefixSum, prefixSum2,transactions]
+		[percentagehash, prefixSum, prefixSumD,transactions, deposits]
 		
 	end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -53,6 +53,13 @@ Client.delete_all
     acc1 = business5.build_account(balance: 0.0, account_id: "aaae", pin: 1234).save
     prof2 = Vprofile.create!(client: business5, businessname: "TestBusiness5 inc.", category: 5)
 
+
+    Transaction.create!(customer_id: customer.id, vendor_id: business.id, amount: 150, state: :success, kind: :deposit)
+    Transaction.create!(customer_id: customer.id, vendor_id: business2.id, amount: 120, state: :success, kind: :deposit)
+    Transaction.create!(customer_id: customer.id, vendor_id: business3.id, amount: 130, state: :success, kind: :deposit)
+    Transaction.create!(customer_id: customer.id, vendor_id: business4.id, amount: 140, state: :success, kind: :deposit)
+    Transaction.create!(customer_id: customer.id, vendor_id: business5.id, amount: 160, state: :success, kind: :deposit)
+
     Transaction.create!(customer_id: customer.id, vendor_id: business.id, amount: 50, state: :success, kind: :purchase)
 	Transaction.create!(customer_id: customer.id, vendor_id: business2.id, amount: 20, state: :success, kind: :purchase)
 	Transaction.create!(customer_id: customer.id, vendor_id: business3.id, amount: 30, state: :success, kind: :purchase)
